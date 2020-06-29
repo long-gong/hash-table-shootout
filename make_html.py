6878475a502fdde41d024222af34da68b8c3871a
Long Gong <long.github@gamil.com>
2020-06-29 01:37:06 -0700

    removed all except html

--- make_html.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-with open("charts-template.html", "r") as fp:
-    html_template = fp.read()
-with open("charts.html", "w") as fp:
-    fp.write(html_template.replace("__CHART_DATA_GOES_HERE__", sys.stdin.read()))
-
